a83ec2b1723e424d46e800012b5d2ff2fc5e7aba
loicelaura <160720402+loicelaura@users.noreply.github.com>
2024-02-23 14:59:50 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# Ask the user for their name using the input() function
+name = input("Enter your name: ")
+
+# Ask the user for their age using the input() function
+age = input("Enter your age: ")
+
+# Ask the user for their location using the input() function
+location = input("Enteryour location: ")
+
+# Construct a personalized message
+message = f"Hello, {name}! It's nice to meet you. You are {age} years old and you are from {location}."
+
+# Print out the personalized message
+print(message)
